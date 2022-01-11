cb3611c535fe0cd8b61bddb681878b4677a49b99
Andrii <remote.makandriival@gmail.com>
2022-01-12 00:11:17 +0200

    hw7done

--- JS-7/task4/task4.js
+++ JS-7/task4/task4.js
@@ -8,3 +8,28 @@
 //   <span id="value">0</span>
 //   <button type="button" data-action="increment">+1</button>
 // </div>
+
+let counterValue = 0;
+class Count {
+    increment () {
+        return counterValue = counterValue + 1;
+    };
+    decrement () {
+        return counterValue = counterValue - 1;
+    };
+};
+let counter = new Count();
+
+let incButton = document.querySelector('.inc');
+let decButton = document.querySelector('.dec');
+let countDisplay = document.querySelector('#value');
+
+
+incButton.addEventListener ( 'click', ()=>{
+    counter.increment();
+    countDisplay.textContent = counterValue;
+});
+decButton.addEventListener ( 'click', ()=>{
+    counter.decrement();
+    countDisplay.textContent = counterValue;
+});
